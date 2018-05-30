539bc8ccbd0d759bbcc3fc5053978cf0b2f9baac
ashutosh0889 <ashutosh.sonaliya@progenbusiness.com>
2018-05-30 19:05:19 +0530

    Changes in Frequency Sort

--- src/com/ds/sorting/FrequencySort.java
+++ src/com/ds/sorting/FrequencySort.java
@@ -5,22 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class FrequencySort {
-    public static void main(String[] args) {
-        int[] ar = new int[] {5,2,8,8,5,5,8,1,9,0,1,1,0,1};
-
-        Map<Integer,Integer> numbers = new HashMap<>();
-
-        for(int number : ar) {
-            if(numbers.containsKey(number)) {
-                Integer  count = numbers.get(number);
-                numbers.put(number, ++count);
-            } else {
-                numbers.put(number,1);
-            }
-        }
-
-        final class FrequencyComparator implements Comparator<Integer> {
+class FrequencyComparator implements Comparator<Integer> {
             Map<Integer,Integer> refMap;
             public FrequencyComparator(Map<Integer,Integer> base) {
                 this.refMap = base;
@@ -38,8 +23,22 @@ public class FrequencySort {
                 // if frequencies are same then compare number itself
                 return  num == 0 ? k1.compareTo(k2)   : num;
             }
-        }
+  
+}
+public class FrequencySort {
+    public static void main(String[] args) {
+        int[] ar = new int[] {5,2,8,8,5,5,8,1,9,0,1,1,0,1};
 
+        Map<Integer,Integer> numbers = new HashMap<>();
+
+        for(int number : ar) {
+            if(numbers.containsKey(number)) {
+                Integer  count = numbers.get(number);
+                numbers.put(number, ++count);
+            } else {
+                numbers.put(number,1);
+            }
+        }
         FrequencyComparator comp = new FrequencyComparator(numbers);
         TreeMap<Integer,Integer> sortedMap = new TreeMap<Integer,Integer>(comp);
         sortedMap.putAll(numbers);
@@ -50,4 +49,6 @@ public class FrequencySort {
             }
         }
     }
+     
 }
+
