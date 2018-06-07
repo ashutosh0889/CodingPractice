3491e2d154ae780d9ac017d79e7ac149a27e31b1
ashutosh0889 <ashutosh.sonaliya@progenbusiness.com>
2018-06-07 13:10:09 +0530

    changed variable name in CustomArrayList.

--- src/com/corejava/miscellaneous/CustomArrayList.java
+++ src/com/corejava/miscellaneous/CustomArrayList.java
@@ -8,38 +8,38 @@ import java.util.Arrays;
  */
 public class CustomArrayList<E> {
     private final int INITIAL_CAPACITY = 10;
-    private Object []dataArr;
+    private Object []elementData;
     int size = 0;
     
     CustomArrayList(){
-        dataArr = new Object[INITIAL_CAPACITY];
+        elementData = new Object[INITIAL_CAPACITY];
     }
     public void reSize(){
         int newCapacity = (Integer)INITIAL_CAPACITY + INITIAL_CAPACITY * 3/2;
-        dataArr = Arrays.copyOf(dataArr, newCapacity);
+        elementData = Arrays.copyOf(elementData, newCapacity);
     }
     public void add(E obj){
-        if(size == dataArr.length)
+        if(size == elementData.length)
             reSize();
-        dataArr[size++] = obj;
+        elementData[size++] = obj;
     }
     public E get(int index){
         if(index < 0 || index > size)
             throw new IndexOutOfBoundsException("Index not found");
-        return (E)dataArr[index];
+        return (E)elementData[index];
     }
     public E remove(int index){
         if(index < 0 || index > size)
             throw new IndexOutOfBoundsException("Index not found");
-        E temp = (E)dataArr[index];
+        E temp = (E)elementData[index];
         for(int i= index; i< size; i++){
-            dataArr[i] = dataArr[i+1];
+            elementData[i] = elementData[i+1];
         }
         size--;
         return temp;
     }
     public void diaplay(){
-        for(Object o : dataArr){
+        for(Object o : elementData){
             if(o != null)
                 System.out.println(" "+o+" ");
         }
