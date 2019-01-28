1cf36ab5758b4ba11c3d0ca7ce217abd715f15e2
ashutosh <ashutosh@DESKTOP-IL1RVV0>
2019-01-28 16:35:39 +0530

    Implemented Singular Link list.

---
+++ src/com/ds/linklist/singular/Node.java
@@ -0,0 +1,15 @@
+package com.ds.linklist.singular;
+
+/**
+ *
+ * @author ashutosh
+ */
+public class Node<T> {
+    T data;
+    Node next;
+    
+    Node(T data){
+        this.data = data;
+        this.next = null;
+    }
+}
--- src/com/ds/linklist/singular/SingularLinkList.java
+++ src/com/ds/linklist/singular/SingularLinkList.java
@@ -4,6 +4,68 @@ package com.ds.linklist.singular;
  *
  * @author ashutosh
  */
-public class SingularLinkList {
-    
+public class SingularLinkList<T> {
+
+    Node root;
+
+    public void insert(T data) {
+        if (root == null) {
+            root = new Node(data);
+        } else {
+            Node current = root;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new Node(data);
+        }
+    }
+
+    public void insertAfter(T data) {
+        Node node = new Node(data);
+
+        if (root == null) {
+            root = node;
+        } else {
+            node.next = root;
+            root = node;
+        }
+    }
+
+    public void delete(T data) {
+        if (data == null || root == null) {
+            return;
+        } else {
+            Node current = root, prev = null;
+            if (current != null && current.data.equals(data)) {
+                root = current.next;
+            } else {
+                while (current != null && !current.data.equals(data)) {
+                    prev = current;
+                    current = current.next;
+                }
+                prev.next = current.next;
+            }
+
+        }
+    }
+
+    public void printList() {
+        Node current = root;
+        while (current != null) {
+            System.out.print(current.data + "    ");
+            current = current.next;
+        }
+    }
+}
+
+class TestSingularLinkList {
+
+    public static void main(String[] args) {
+        SingularLinkList s1 = new SingularLinkList();
+        s1.insertAfter(10);
+        s1.insertAfter(20);
+        s1.insertAfter(30);
+        s1.delete(30);
+        s1.printList();
+    }
 }
